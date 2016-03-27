f64e05b630f7690bd57decf9e185081dc309a904
Tudor Văran <tudor.varan@yahoo.com>
2016-03-27 23:48:14 +0300

    Updated proect (1.1)
    Added new supported formats:
    .sh
    .c
    .cpp
    .txt
    .zip
    .pdf
    .h

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
 		error_reporting(E_ALL | E_STRICT);
 		ini_set('display_errors', 'On');
 		$files = scandir ( "." , 1);
+		$new_files = array();
+		for ($i = 0; $i < count($files); $i++) 
+			if ($files[$i] != "question.gif" and $files[$i] != "index.php" and $files[$i] != "default.css" and $files[$i] != "folder.png" and $files[$i] != "." and $files[$i] != "pdf.png" and $files[$i] != "h.png" and $files[$i] != "c.png" and $files[$i] != "zip.png" and $files[$i] != "cpp.png" and $files[$i] != "sh.png" and $files[$i] != "txt.png")
+			array_push ($new_files, $files[$i]);
+		$files = $new_files;			
 	?>
 	<body >
 		<?php
@@ -27,6 +32,27 @@
 								if (strpos($files[$i * 5 + $j], ".png") != false or strpos($files[$i * 5 + $j], ".jpg") != false or strpos($files[$i * 5 + $j], ".jpeg") != false)
 									echo "<img id = \"file_image\" src = \"".$files[$i * 5 + $j]."\" > ";
 							else
+								if (strpos($files[$i * 5 + $j], ".pdf") != false)
+									echo "<img id = \"file_image\" src = \"pdf.png\"> ";
+							else
+								if (strpos($files[$i * 5 + $j], ".h") != false)
+									echo "<img id = \"file_image\" src = \"h.png\"> ";							
+							else	
+								if (strpos($files[$i * 5 + $j], ".cpp") != false)
+									echo "<img id = \"file_image\" src = \"cpp.png\"> ";
+							else	
+								if (strpos($files[$i * 5 + $j], ".sh") != false)
+									echo "<img id = \"file_image\" src = \"sh.png\"> ";
+							else	
+								if (strpos($files[$i * 5 + $j], ".zip") != false)
+									echo "<img id = \"file_image\" src = \"zip.png\"> ";
+							else
+								if (strpos($files[$i * 5 + $j], ".c") != false)
+									echo "<img id = \"file_image\" src = \"c.png\"> ";
+							else
+								if (strpos($files[$i * 5 + $j], ".txt") != false)
+									echo "<img id = \"file_image\" src = \"txt.png\"> ";
+							else
 								echo "<img id = \"file_image\" src = \"question.gif\" >"; ?>
 						</a>
 					</td>			
@@ -36,7 +62,7 @@
 			<?php
 			for($j = 0; $j < min(5, count($files) - $i * 5); $j = $j + 1) {
 				?>					
-				<td id = "files_text_data">
+				<td id = "files_text_data" align = "center">
 					<?php echo "<a href = \"".$files[$i * 5 + $j]."\" >" .$files[$i * 5 + $j]. "</a>"; ?>
 				</td>																					
 					<?php
